c126295d12efab796e08ebafd9a453cc785c8f20
Khristian Gris <74702918+KhristianGris@users.noreply.github.com>
2025-03-26 13:52:22 -0700

    ID Links
    Cambio de ID de links internos, y limpieza de código.

--- Js/Script.js
+++ Js/Script.js
@@ -71,64 +71,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-// CONTROL DE VOLUMEN EN #MAIN-SLIDER
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Selecciona el slider #main-slider
-//   const mainSlider = document.querySelector('#main-slider');
-
-//   // Crea el contenedor de la barra de volumen
-//   const volumeContainer = document.createElement('div');
-//   volumeContainer.classList.add('volume-container');
-
-//   // Estilo del contenedor
-//   volumeContainer.style.position = 'absolute';
-//   volumeContainer.style.top = '50%';
-//   volumeContainer.style.left = '-40px'; // Posiciona fuera del slider a la izquierda
-//   volumeContainer.style.transform = 'translateY(-50%)';
-//   volumeContainer.style.display = 'flex';
-//   volumeContainer.style.flexDirection = 'column';
-//   volumeContainer.style.alignItems = 'center';
-//   volumeContainer.style.padding = '10px';
-//   volumeContainer.style.zIndex = '10';
-
-//   // Crea la barra de volumen
-//   const volumeSlider = document.createElement('input');
-//   volumeSlider.type = 'range';
-//   volumeSlider.min = '0';
-//   volumeSlider.max = '1';
-//   volumeSlider.step = '0.1';
-//   volumeSlider.value = '0.5'; // Valor inicial
-//   volumeSlider.style.writingMode = 'bt-lr'; // Hace que la barra sea vertical
-//   volumeSlider.style.transform = 'rotate(-90deg)'; // Rota la barra para que sea vertical
-//   volumeSlider.style.width = '100px';
-//   volumeSlider.style.margin = '10px 0';
-//   volumeSlider.style.cursor = 'pointer';
-
-//   // Evento para ajustar el volumen de los videos dentro del slider
-//   volumeSlider.addEventListener('input', () => {
-//     const videos = mainSlider.querySelectorAll('video'); // Selecciona todos los videos dentro del slider
-//     videos.forEach((video) => {
-//       video.volume = volumeSlider.value; // Ajusta el volumen según el valor de la barra
-//     });
-//   });
-
-//   // Agrega la barra al contenedor
-//   volumeContainer.appendChild(volumeSlider);
-
-//   // Cambia la posición del contenedor al lado derecho
-//   volumeContainer.style.left = 'auto';
-//   volumeContainer.style.right = '-40px'; // Posiciona fuera del slider a la derecha
-
-//   // Agrega el contenedor al slider
-//   mainSlider.style.position = 'relative'; // Asegura que el contenedor se posicione correctamente
-//   mainSlider.appendChild(volumeContainer);
-// });
-
-
-
-
-
 // LOADER MINIMALISTA
 document.addEventListener('DOMContentLoaded', function () {
   // Crear el contenedor del loader
